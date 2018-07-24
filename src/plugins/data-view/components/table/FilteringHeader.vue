664ecc9bc90620b5e68581ecd89381d5efd171a9
fish-ball <57082212@qq.com>
2018-07-24 15:19:38 +0800

    支持了 TableHeaderField 的 filtering 采用数组方式为同一个字段指定多个过滤器:

--- src/plugins/data-view/components/table/FilteringHeader.vue
+++ src/plugins/data-view/components/table/FilteringHeader.vue
@@ -21,25 +21,39 @@
       // 获取 filtering 选项参数，如果指定为字符串，自动撸成 keyword 类型
       const options = (typeof vm.field.filtering === 'string')
         ? { key: vm.field.filtering } : vm.field.filtering
-      if ((options.type || 'keyword') === 'keyword') { // 关键词类型
-        return h(FilteringHeaderKeyword, {
-          props: { field: vm.field, options }
-        })
-      } else if (options.type === 'select') { // 选项类型
-        return h(FilteringHeaderSelect, {
-          props: { field: vm.field, options }
-        })
-      } else if (options.type === 'range') { // 数字范围类型
-        return h(FilteringHeaderRange, {
-          props: { field: vm.field, options }
-        })
-      } else if (options.type === 'date-range') { // 日期范围类型
-        return h(FilteringHeaderDateRange, {
-          props: { field: vm.field, options }
-        })
-      } else {
-        console.error('未定义的 filtering 类型', vm.field)
-        return h('div', '未定义的 filtering 类型')
+      // 如果 filtering 为数组，则认为提供了连续多个筛选器，递归渲染
+      if (options instanceof Array) {
+        return h('div', {
+          style: { display: 'inline-block' }
+        }, options.map(opt => vm.renderWithFilteringOption(h, opt)))
+      }
+      // 否则直接返回单个渲染
+      return vm.renderWithFilteringOption(h, options)
+    },
+    methods: {
+      renderWithFilteringOption (h, opt) {
+        // 单个渲染
+        const vm = this
+        if ((opt.type || 'keyword') === 'keyword') { // 关键词类型
+          return h(FilteringHeaderKeyword, {
+            props: { field: vm.field, options: opt }
+          })
+        } else if (opt.type === 'select') { // 选项类型
+          return h(FilteringHeaderSelect, {
+            props: { field: vm.field, options: opt }
+          })
+        } else if (opt.type === 'range') { // 数字范围类型
+          return h(FilteringHeaderRange, {
+            props: { field: vm.field, options: opt }
+          })
+        } else if (opt.type === 'date-range') { // 日期范围类型
+          return h(FilteringHeaderDateRange, {
+            props: { field: vm.field, options: opt }
+          })
+        } else {
+          console.error('未定义的 filtering 类型', vm.field)
+          return h('div', '未定义的 filtering 类型')
+        }
       }
     }
   }
