885501cf32a444bef2c2fbda6661daa74ab09b66
fish-ball <57082212@qq.com>
2020-02-02 17:29:11 +0800

    修正图片预览 z-index 过低被 ivu modal 挡住的问题

--- src/plugins/data-view/components/table/FilteringHeaderSelect.vue
+++ src/plugins/data-view/components/table/FilteringHeaderSelect.vue
@@ -9,7 +9,8 @@
       const vm = this
       const key = vm.options.key
       const choices = vm.options.choices
-      const value = vm.field.$view.query[key] || ''
+      let value = vm.field.$view.query[key] || ''
+      if (vm.options.refiner) value = vm.options.refiner(value)
 
       if (value) {
         return h('tag', {
