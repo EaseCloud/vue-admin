e486ebb07961819c780d2b2c785763f660775e64
fish-ball <57082212@qq.com>
2020-12-08 12:58:27 +0800

    添加了部分用例功能

--- src/plugins/data-view/components/EditView.vue
+++ src/plugins/data-view/components/EditView.vue
@@ -40,129 +40,144 @@
 </template>
 
 <script>
-import EditViewForm from './EditViewForm.vue'
+  import EditViewForm from './EditViewForm.vue'
 
-export default {
-  name: 'EditView',
-  props: {
-    ...EditViewForm.props
-  },
-  computed: {
-    editViewOptions () {
-      const vm = this
+  export default {
+    name: 'EditView',
+    props: {
+      ...EditViewForm.props
+    },
+    computed: {
+      editViewOptions () {
+        const vm = this
+        return {
+          ...vm.$attrs,
+          ...vm.$props,
+          id: Number(vm.$props.id || vm.$route.params.id)
+        }
+      },
+      item () {
+        const vm = this
+        return vm.$refs.form.item
+      }
+    },
+    data () {
       return {
-        ...vm.$attrs,
-        ...vm.$props,
-        id: Number(vm.$props.id || vm.$route.params.id)
+        cacheName: '',
+        cachePath: ''
       }
     },
-    item () {
-      const vm = this
-      return vm.$refs.form.item
-    }
-  },
-  data () {
-    return {
-      cacheName: '',
-      cachePath: ''
-    }
-  },
-  methods: {
-    async refresh () {
-      const vm = this
-      const form = await vm.waitFor(vm.$refs, 'form')
-      if (vm.editViewOptions.options.onRefresh) {
-        vm.editViewOptions.options.onRefresh.apply(vm, vm.item)
-      } else {
-        await form.reload()
-      }
-    },
-    async save () {
-      // TODO: 提交之前应该实现 validate 验证方法，以校验 required 等字段的情况
-      const vm = this
-      const isCreate = !vm.$refs.form.id_
-      await vm.validate()
-      await vm.$refs.form.save()
-      // 保存之后
-      if (isCreate) {
-        // 如果是创建要跳转页面
-        const route = await vm.getModelEditRoute(vm.model, vm.$refs.form.id_)
-        await vm.replacePage(route)
-        vm.$router.push(route)
-      }
-      await vm.refresh()
-    },
-    async submit () {
-      const vm = this
-      await vm.save()
-      vm.closeCurrentPage()
-    },
-    async remove () {
-      const vm = this
-      await vm.$confirm('确认删除？')
-      await vm.$refs.form.deleteItem()
-      vm.closeCurrentPage()
-    },
-    async validate () {
-      const vm = this
-      return vm.$refs.form.validate()
-    }
-  },
-  mounted () {
-    const vm = this
-    vm.cacheName = vm.$route.name
-    vm.cachePath = vm.$route.path
-  },
-  watch: {
-    $route (to) {
-      const vm = this
-      // 使用 cacheName 和 cachePage 缓存属性来避免同组件跳转时的内容不更新
-      if (to.name === vm.cacheName && to.path !== vm.cachePath) {
-        vm.cachePath = to.path
-        vm.$nextTick(() => {
-          vm.$refs.form.reload()
+    methods: {
+      async refresh () {
+        const vm = this
+        const form = await vm.waitFor(vm.$refs, 'form')
+        if (vm.editViewOptions.options.onRefresh) {
+          vm.editViewOptions.options.onRefresh.apply(vm, vm.item)
+        } else {
+          await form.reload()
+        }
+      },
+      async save () {
+        // TODO: 提交之前应该实现 validate 验证方法，以校验 required 等字段的情况
+        const vm = this
+        const isCreate = !vm.$refs.form.id_
+        await vm.validate()
+        await vm.$refs.form.save()
+        // 保存之后
+        if (isCreate) {
+          // 如果是创建要跳转页面
+          const route = await vm.getModelEditRoute(vm.model, vm.$refs.form.id_)
+          await vm.replacePage(route)
+          vm.$router.push(route)
+        }
+        await vm.refresh()
+      },
+      async submit () {
+        const vm = this
+        await vm.save()
+        vm.closeCurrentPage()
+      },
+      async remove () {
+        const vm = this
+        await vm.$confirm('确认删除？')
+        await vm.$refs.form.deleteItem()
+        vm.closeCurrentPage()
+      },
+      async validate () {
+        const vm = this
+        return vm.$refs.form.validate()
+      },
+      async refreshField (id) {
+        const vm = this
+        let index = -1
+        vm.fields.forEach((f, i) => {
+          if (f.key === id || f.id === id) {
+            index = i
+          }
         })
+        if (index > -1) {
+          const field = vm.fields.splice(index, 1)
+          await vm.$nextTick(() => {
+            vm.fields.splice(index, 0, ...field)
+          })
+        }
+      }
+    },
+    mounted () {
+      const vm = this
+      vm.cacheName = vm.$route.name
+      vm.cachePath = vm.$route.path
+    },
+    watch: {
+      $route (to) {
+        const vm = this
+        // 使用 cacheName 和 cachePage 缓存属性来避免同组件跳转时的内容不更新
+        if (to.name === vm.cacheName && to.path !== vm.cachePath) {
+          vm.cachePath = to.path
+          vm.$nextTick(() => {
+            vm.$refs.form.reload()
+          })
+        }
       }
     }
   }
-}
 </script>
 
 <style lang="less" scoped>
-@import "../../../style/defines";
+  @import "../../../style/defines";
 
-.page-content.edit-view {
-  position: absolute;
-  top: 10px;
-  left: 10px;
-  right: 10px;
-  bottom: 10px;
-  padding-bottom: 60px;
-  .page-header {
-    .clearfix();
-    .title {
-      .title();
-      display: inline-block;
-      line-height: 32px;
-    }
-    .subtitle {
-      .text();
-      display: inline-block;
-      line-height: 32px;
-      margin-left: 1em;
-    }
-    .controls {
-      float: right;
+  .page-content.edit-view {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    right: 10px;
+    bottom: 10px;
+    padding-bottom: 60px;
+    .page-header {
+      .clearfix();
+      .title {
+        .title();
+        display: inline-block;
+        line-height: 32px;
+      }
+      .subtitle {
+        .text();
+        display: inline-block;
+        line-height: 32px;
+        margin-left: 1em;
+      }
+      .controls {
+        float: right;
+      }
     }
   }
-}
 
-.edit-view /deep/ .ivu-card-body {
-  position: absolute;
-  top: 62px;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  overflow: auto;
-}
+  .edit-view /deep/ .ivu-card-body {
+    position: absolute;
+    top: 62px;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    overflow: auto;
+  }
 </style>
