15bb09feb33aca9ef1e815a4a4bc990b67bfcc9f
fish-ball <57082212@qq.com>
2020-12-15 09:23:02 +0800

    在 FormFieldRender 中支持了 async renderData 异步抓取内容

--- src/plugins/data-view/components/form/FormFieldRender.vue
+++ src/plugins/data-view/components/form/FormFieldRender.vue
@@ -1,30 +1,43 @@
+<template>
+  <render-component v-if="renderFunction" :render="renderFunction"></render-component>
+</template>
 <script>
   export default {
     name: 'FormFieldRender',
     props: {
       field: {
         type: Object,
-        default: () => {
-        }
+        default: () => ({
+          async renderData (field, item, data) {
+            return [field, item, data]
+          },
+          render (h) {
+            return h('div', '请在 field 中实现 render(h, field, item, data) 方法')
+          }
+        })
       }
     },
     data () {
       return {
         component: null,
-        data: []
+        data: [],
+        renderFunction: null
       }
     },
-    render (h) {
-      const vm = this
-      const component = vm.field.render(h, vm.field, vm.field.context.item, vm.data)
-      vm.waitFor(component, 'componentInstance').then(componentInstance => {
-        vm.field.component = componentInstance
-      })
-      return component
-    },
-    mounted () {
+    async mounted () {
       const vm = this
       vm.field.$el = this
+      let renderData = [vm.field, vm.field.context.item, vm.data]
+      if (vm.field.renderData) {
+        renderData = await vm.field.renderData(vm.field, vm.field.context.item, vm.data)
+      }
+      vm.renderFunction = function (h) {
+        const component = vm.field.render(h, ...renderData)
+        vm.waitFor(component, 'componentInstance').then(componentInstance => {
+          vm.field.component = componentInstance
+        })
+        return component
+      }
     },
     methods: {
       async reload () {
