f1d7d7f67cd7cd476e7d5b268f76869361801db9
fish-ball <57082212@qq.com>
2021-07-07 15:12:09 +0800

    Optimize FormFieldItemList, support full inline edit

--- src/plugins/data-view/components/form/FormFieldItemList.vue
+++ src/plugins/data-view/components/form/FormFieldItemList.vue
@@ -1,22 +1,78 @@
 <template>
   <div class="field-item field-item-item-list"
        :style="{width: !!field.final.width && field.final.width}">
-    <i-table ref="table" border v-if="initialized" :columns="columns"
-             size="small" :data="items">
-      <template slot-scope="{row, index}" slot="__action__">
-        <!-- TODO: 后续如果有需要可以加上类似的 actions 接口 -->
-        <!--<template v-for="(action, i) in field.actions"-->
-        <!--v-if="$refs.form && (action.display===void 0||finalizeSync(action.display, items.item))">-->
-        <!--<i-button :key="i"-->
-        <!--:type="action.buttonType"-->
-        <!--@click="doAction(action.action, [$refs.form.item])">{{action.label}}-->
-        <!--</i-button>-->
-        <!--<i :key="'_'+i">&lt;!&ndash;避免按钮之间粘在一起&ndash;&gt;</i>-->
-        <!--</template>-->
-        <i-button size="small" @click="editItem(index)">编辑</i-button>
-        <i-button size="small" @click="removeItem(index)">删除</i-button>
-      </template>
-    </i-table>
+    <div>
+    </div>
+    <div class="ivu-table-wrapper ivu-table-wrapper-with-border">
+      <div class="ivu-table ivu-table-small ivu-table-border">
+        <div class="ivu-table-header">
+          <table cellspacing="0" cellpadding="0" border="0" style="width: 599px;">
+            <thead>
+            <tr>
+              <th class="ivu-table-column-2Gkwny" v-if="!rawMode">
+                <div class="ivu-table-cell">
+                  <div>Key</div>
+                </div>
+              </th>
+              <th class="ivu-table-column-CJnaEd" v-if="!rawMode">
+                <div class="ivu-table-cell">
+                  <div>Value</div>
+                </div>
+              </th>
+              <th class="ivu-table-column-2ZMNEV">
+                <div class="ivu-table-cell" v-if="rawMode">
+                  批量编辑，每行一个，格式 [key: val]
+                </div>
+                <div class="ivu-table-cell" v-else>
+                  <i-button size="small" @click="addItem()">添加</i-button>
+                </div>
+                <div class="ivu-table-cell" style="float: right">
+                  <i-switch size="large" v-model="rawMode">
+                    <span slot="open">批量</span>
+                    <span slot="close">列表</span>
+                  </i-switch>
+                </div>
+              </th>
+            </tr>
+            </thead>
+          </table>
+        </div>
+        <div class="ivu-table-body" v-show="!rawMode">
+          <table cellspacing="0" cellpadding="0" border="0" style="width: 599px;">
+            <tbody class="ivu-table-tbody">
+            <tr draggable="false" class="ivu-table-row" v-for="(item, index) in items">
+              <td class="ivu-table-column-2Gkwny">
+                <div class="ivu-table-cell">
+                  <span>{{item.key}}</span>
+                </div>
+              </td>
+              <td class="ivu-table-column-CJnaEd">
+                <div class="ivu-table-cell">
+                  <span>{{item.value}}</span>
+                </div>
+              </td>
+              <td class="ivu-table-column-2ZMNEV">
+                <div class="ivu-table-cell">
+                  <div class="ivu-table-cell-slot">
+                    <i-button size="small" @click="editItem(index)">编辑</i-button>
+                    <i-button size="small" @click="removeItem(index)">删除</i-button>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="code-block" style="line-height: 20px">
+      <codemirror v-if="rawMode"
+                  class="form-field-input"
+                  ref="input"
+                  v-model="rawText"
+                  :options="{theme: 'default', ...(field.codeOptions || {})}"/>
+
+    </div>
   </div>
 </template>
 
@@ -32,9 +88,31 @@
     },
     data () {
       return {
-        initialized: false,
         items: [],
-        columns: []
+        rawMode: false
+      }
+    },
+    computed: {
+      rawText: {
+        get () {
+          const vm = this
+          return vm.items.map(x => `${x.key}: ${x.value}`).join('\n')
+        },
+        set (value) {
+          const vm = this
+          console.log(arguments)
+          const items = []
+          value.split('\n').forEach(line => {
+            const pos = line.indexOf(':')
+            if (pos === -1) return
+            const key = line.substr(0, pos).trim()
+            const value = line.substr(pos + 1).trim()
+            if (!key) return
+            items.push({key, value})
+          })
+          vm.items = items
+          vm.$emit('input', vm.items)
+        }
       }
     },
     mounted () {
@@ -46,28 +124,7 @@
     methods: {
       async reload () {
         const vm = this
-        vm.initialized = false
-        await vm.$nextTick()
         vm.items = vm.field.value
-        const cols = vm.field.fields.map(({key, label}) => ({
-          key,
-          title: label,
-          renderHeader (h, {column, index}) {
-            return h('div', column.title)
-          }
-        }))
-        cols.push({
-          title: '操作',
-          slot: '__action__',
-          renderHeader (h, {column, index}) {
-            return h('i-button', {
-              props: {size: 'small'},
-              on: {click: () => vm.addItem()}
-            }, '添加')
-          },
-        })
-        vm.columns = cols
-        vm.initialized = true
       },
       async addItem () {
         const vm = this
@@ -87,7 +144,7 @@
           ...field, default: item[field.key]
         }))
         item = await vm.modalForm({fields}, {title: '修改对象'})
-        vm.items.splice(index)
+        vm.items.splice(index, 1, item)
         vm.$emit('input', vm.items)
       }
     }
