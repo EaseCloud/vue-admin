4226c9048750d52c97a2a4fa8aa57781de9ad0ef
fish-ball <57082212@qq.com>
2022-02-19 19:49:31 +0800

    魔改 quill-image-extend-module，解决各种粘贴不顺的问题

--- src/libs/quill-image-extend-module/index.js
+++ src/libs/quill-image-extend-module/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 /**
  *@description 观察者模式 全局监听富文本编辑器
  */
@@ -27,19 +29,101 @@ export class ImageExtend {
    * config  keys: action, headers, editForm start end error  size response
    */
   constructor (quill, config = {}) {
-    this.id = Math.random()
-    this.quill = quill
-    this.quill.id = this.id
-    this.config = config
-    this.file = ''  // 要上传的图片
-    this.imgURL = ''  // 图片地址
-    quill.root.addEventListener('paste', this.pasteHandle.bind(this), false)
-    quill.root.addEventListener('drop', this.dropHandle.bind(this), false)
+    const self = this
+    window.quill = quill
+    window.ext = self
+    self.id = Math.random()
+    self.quill = quill
+    self.quill.id = self.id
+    self.config = config
+    self.file = ''  // 要上传的图片
+    self.imgURL = ''  // 图片地址
+    self.selection = null
+    // https://quilljs.com/docs/api/#editor-change
+    // quill.on('editor-change', function (eventName, ...args) {
+    //   console.log('editor-change', eventName, args)
+    //   console.log('select', quill.getSelection())
+    //   self.selection = quill.getSelection() || self.selection
+    // })
+    quill.root.addEventListener('paste', self.pasteHandle.bind(self), false)
+    quill.root.addEventListener('drop', self.dropHandle.bind(self), false)
     quill.root.addEventListener('dropover', function (e) {
       e.preventDefault()
     }, false)
-    this.cursorIndex = 0
-    QuillWatch.on(this.id, this)
+    self.cursorIndex = 0
+    QuillWatch.on(self.id, self)
+  }
+
+  /**
+   * 处理粘贴事件，几种情况
+   * 1. 粘贴截图，仅有一个元素：
+   *    dataTransfer.items = [{kind: 'file', type: 'image/png'}]
+   * 2. 粘贴多个文件，几个文件就有几个元素，全部的 kind 都是 file：
+   *    dataTransfer.item = [{kind: 'file', type: '实际MIMIE'}, ...]
+   * 3. 粘贴纯文本，仅有一个元素：
+   *    dataTransfer.item = [{kind: 'string', type: 'text/plain'}]
+   * 4. 粘贴富文本内容（网页内容），有且仅有两个元素：
+   *    dataTransfer.item = [{kind: 'string', type: 'text/plain'},
+   *                         {kind: 'string', type: 'text/html'}]
+   *    4.1. 第一个为 html 内容强制转换为纯文本的内容结果
+   *    4.2. 第二个为 html 内容的 Markup
+   * 5. 粘贴RTF内容（Word内容），有且仅有三个元素：
+   *    dataTransfer.item = [{kind: 'string', type: 'text/plain'},
+   *                         {kind: 'string', type: 'text/html'},
+   *                         {kind: 'string', type: 'text/rtf'}]
+   *    5.1. 第一个为 html 内容强制转换为纯文本的内容结果
+   *    5.2. 第二个为 html 内容的 Markup（一个 html markup，但是标签好怪）
+   *    5.3. 第三个为 RTF 实际内容为一个 rtf 文件的 markup（不懂不处理）
+   * 6. 粘贴RTF内容（Excel内容），有且仅有四个元素：
+   *    dataTransfer.item = [{kind: 'string', type: 'text/plain'},
+   *                         {kind: 'string', type: 'text/html'},
+   *                         {kind: 'string', type: 'text/rtf'},
+   *                         {kind: 'file', type: 'image/png'}]
+   *    6.1. 第一个为 html 内容强制转换为纯文本的内容结果
+   *    6.2. 第二个为 html 内容的 Markup（一个 html markup，格式：html.head.style/html.body.table）
+   *    6.3. 第三个为 RTF 实际内容为一个 rtf 文件的 markup（不懂不处理）
+   *    6.4. 第四个为图片，应该是 Excel 的选中区域截图
+   * 获取内容几种情况：
+   * 1. 如果是文件，通过 dataTransfer.items[i].getAsFile()
+   * 2. 如果是其他类型内容，通过 dataTransfer.getData(dataTransfer.items[i].type)
+   *
+   * @param e
+   * @param dataTransfer
+   */
+  handleData (e, dataTransfer) {
+    // console.log('>>> handleData')
+    let self = this
+    QuillWatch.emit(self.quill.id, 0)
+    if (!dataTransfer.items.length) return
+    // 第一第二种粘贴图片的分支
+    // TODO: 可以考虑集成文件的上传
+    if (dataTransfer.items[0].kind === 'file') {
+      _.forEach(dataTransfer.items, item => {
+        if (item.kind === 'file' && item.type.match(/^image\//i)) {
+          self.file = item.getAsFile()
+          // 如果图片限制大小
+          if (self.config.size && self.file.size >= self.config.size * 1024 * 1024) {
+            if (self.config.sizeError) {
+              self.config.sizeError()
+            }
+            return
+          }
+          if (self.config.action) {
+            self.uploadImg()
+          } else {
+            self.toBase64()
+          }
+        }
+        e.preventDefault()
+      })
+    } else if (dataTransfer.items[0].kind === 'string') {
+      // TODO: 精细化的复合处理
+      // 可以预期的第三第四第五种情况
+      self.quill.insertText(self.selection.index, dataTransfer.getData(items[0].type))
+      e.preventDefault()
+    } else {
+      // TODO: 未预期的其他情况，直接默认处理
+    }
   }
 
   /**
@@ -47,44 +131,8 @@ export class ImageExtend {
    * @param e
    */
   pasteHandle (e) {
-    e.preventDefault()
-    QuillWatch.emit(this.quill.id, 0)
-    let clipboardData = e.clipboardData
-    let i = 0
-    let items, item, types
-
-    if (clipboardData) {
-      items = clipboardData.items;
-
-      if (!items) {
-        return;
-      }
-      item = items[0];
-      types = clipboardData.types || [];
-
-      for (; i < types.length; i++) {
-        if (types[i] === 'Files') {
-          item = items[i];
-          break;
-        }
-      }
-      if (item && item.kind === 'file' && item.type.match(/^image\//i)) {
-        this.file = item.getAsFile()
-        let self = this
-        // 如果图片限制大小
-        if (self.config.size && self.file.size >= self.config.size * 1024 * 1024) {
-          if (self.config.sizeError) {
-            self.config.sizeError()
-          }
-          return
-        }
-        if (this.config.action) {
-          this.uploadImg()
-        } else {
-          this.toBase64()
-        }
-      }
-    }
+    // console.log('>>> pasteHandle')
+    this.handleData(e, e.clipboardData)
   }
 
   /**
@@ -92,28 +140,15 @@ export class ImageExtend {
    * @param e
    */
   dropHandle (e) {
-    QuillWatch.emit(this.quill.id, 0)
-    const self = this
-    e.preventDefault()
-    // 如果图片限制大小
-    if (self.config.size && self.file.size >= self.config.size * 1024 * 1024) {
-      if (self.config.sizeError) {
-        self.config.sizeError()
-      }
-      return
-    }
-    self.file = e.dataTransfer.files[0]; // 获取到第一个上传的文件对象
-    if (this.config.action) {
-      self.uploadImg()
-    } else {
-      self.toBase64()
-    }
+    // console.log('>>> dropHandle')
+    this.handleData(e, e.dataTransfer)
   }
 
   /**
    * @description 将图片转为base4
    */
   toBase64 () {
+    // console.log('>>> toBase64')
     const self = this
     const reader = new FileReader()
     reader.onload = (e) => {
@@ -128,6 +163,7 @@ export class ImageExtend {
    * @description 上传图片到服务器
    */
   uploadImg () {
+    // console.log('>>> uploadImg')
     const self = this
     let quillLoading = self.quillLoading
     let config = self.config
@@ -171,8 +207,6 @@ export class ImageExtend {
     // 开始上传数据
     xhr.upload.onloadstart = function (e) {
       QuillWatch.active.uploading()
-      // let length = (self.quill.getSelection() || {}).index || self.quill.getLength()
-      // self.quill.insertText(length, '[uploading...]', { 'color': 'red'}, true)
       if (config.start) {
         config.start()
       }
@@ -202,41 +236,56 @@ export class ImageExtend {
    * @description 往富文本编辑器插入图片
    */
   insertImg () {
+    // console.log('>>> insertImg')
     const self = QuillWatch.active
-    self.quill.insertEmbed(QuillWatch.active.cursorIndex, 'image', self.imgURL)
+    const selection = self.quill.getSelection()
+    self.quill.insertEmbed(selection.index, 'image', self.imgURL)
     self.quill.update()
-    self.quill.setSelection(self.cursorIndex + 1);
+    // self.quill.setSelection(self.cursorIndex + 1);
+    self.quill.setSelection(selection.index + 1);
   }
 
   /**
    * @description 显示上传的进度
    */
   progress (pro) {
-    pro = '[' + 'uploading' + pro + ']'
-    QuillWatch.active.quill.root.innerHTML
-      = QuillWatch.active.quill.root.innerHTML.replace(/\[uploading.*?\]/, pro)
+    // console.log('>>> progress')
+    // 下面这句 replace 会干扰 selection，还不如干掉
+    // pro = '[' + 'uploading' + pro + ']'
+    // QuillWatch.active.quill.root.innerHTML
+    //   = QuillWatch.active.quill.root.innerHTML.replace(/\[uploading.*?\]/, pro)
   }
 
   /**
    * 开始上传
    */
   uploading () {
-    let length = (QuillWatch.active.quill.getSelection() || {}).index || QuillWatch.active.quill.getLength()
-    QuillWatch.active.cursorIndex = length
-    QuillWatch.active.quill.insertText(QuillWatch.active.cursorIndex, '[uploading...]', {'color': 'red'}, true)
+    // console.log('>>> uploading')
+    // 下面这句 replace 会干扰 selection，还不如干掉
+    // let length = (QuillWatch.active.quill.getSelection() || {}).index || QuillWatch.active.quill.getLength()
+    // QuillWatch.active.cursorIndex = length
+    // QuillWatch.active.quill.insertText(QuillWatch.active.cursorIndex, '[uploading...]', {'color': 'red'}, true)
+    // const self = QuillWatch.active
+    // self.quill.insertText(self.selection.index, '[uploading...]', {'color': 'red'}, true)
   }
 
   /**
    * 上传失败
    */
   uploadError () {
-    QuillWatch.active.quill.root.innerHTML
-      = QuillWatch.active.quill.root.innerHTML.replace(/\[uploading.*?\]/, '[upload error]')
+    // console.log('>>> uploadError')
+    // TODO: 插入位置处理有问题
+    // const self = QuillWatch.active
+    // self.quill.root.innerHTML
+    //   = QuillWatch.active.quill.root.innerHTML.replace(/\[uploading.*?\]/, '[upload error]')
   }
 
   uploadSuccess () {
-    QuillWatch.active.quill.root.innerHTML
-      = QuillWatch.active.quill.root.innerHTML.replace(/\[uploading.*?\]/, '')
+    // console.log('>>> uploadSuccess')
+    // TODO: 插入位置处理有问题
+    // const self = QuillWatch.active
+    // self.quill.root.innerHTML
+    //   = QuillWatch.active.quill.root.innerHTML.replace(/\[uploading.*?\]/, '')
   }
 }
 
@@ -244,6 +293,7 @@ export class ImageExtend {
  * @description 点击图片上传
  */
 export function imgHandler () {
+  // console.log('>>> imgHandler')
   let fileInput = document.querySelector('.quill-image-input');
   if (fileInput === null) {
     fileInput = document.createElement('input');
