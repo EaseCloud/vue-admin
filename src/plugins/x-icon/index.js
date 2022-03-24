e7318f6d48cd70db362faa25c20536e1ccc1c79b
fish-ball <57082212@qq.com>
2022-03-24 15:13:59 +0800

    更新 font-awesome 引用，支持 v4-shims 图标

--- src/plugins/x-icon/index.js
+++ src/plugins/x-icon/index.js
@@ -1,6 +1,7 @@
 import XIcon from './XIcon.vue'
 
-import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/css/v4-shims.min.css'
 
 export default {
   install (Vue) {
